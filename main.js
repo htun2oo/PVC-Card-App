a806bfa45c504a037a5a620cd41194b86ef7f062
htun2oo <htun2oo@gmail.com>
2026-09-05 11:36:30 +0630

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,17 @@
 const { app, BrowserWindow } = require('electron');
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    title: "PVC Card Studio & Product Builder",
+    frame: false,            // OS Standard Frame (Title Bar, Menu Bar) တစ်ခုလုံးကို ဖယ်ထုတ်မည်
+    autoHideMenuBar: true,   // Menu Bar ကို လုံးဝဖျောက်မည်
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
+      nodeIntegration: true
     }
   });
 
   win.loadFile('index.html');
 }
 
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
+app.whenReady().then(createWindow);
